b14a2f88ae75eb8fb0dc8a78260a94c50cbb9ef2
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2026-06-24 18:30:52 +0300

    Create nominations model

--- nominations/models.py
+++ nominations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Nomination(models.Model):
+    full_name = models.CharField(max_length=255)
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    what_she_is_doing = models.TextField()
+    submitted_by_name = models.CharField(max_length=255)
+    submitted_by_email = models.EmailField()
+    reviewed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
