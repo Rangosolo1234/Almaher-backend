96ae5aad96ae680503e8d085d7cc416479f035e7
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2026-06-24 19:25:14 +0300

    Create inquiries viewset

--- inquiries/views.py
+++ inquiries/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Inquiry
+from .serializers import InquirySerializer
 
-# Create your views here.
+class InquiryViewSet(viewsets.ModelViewSet):
+    queryset = Inquiry.objects.all().order_by("-created_at")
+    serializer_class = InquirySerializer
