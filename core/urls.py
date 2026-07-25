e0575fa5eddcba3dd6c06dee241ceb8c2f618a28
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2026-07-25 18:45:55 +0300

    Add endpoint for media files

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 from drf_spectacular.views import (
@@ -37,3 +39,6 @@ urlpatterns = [
     path("api/", include("inquiries.urls")),
     path("api/", include("subscriptions.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
