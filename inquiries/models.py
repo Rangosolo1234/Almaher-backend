4a30826cec4726f53b788a9de1a7936667633e96
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2026-08-15 18:57:03 +0300

    Clean inquiries modles file

--- inquiries/models.py
+++ inquiries/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
-
 class Inquiry(models.Model):
     GENERAL = "general"
     PHOTOGRAPHER = "photographer"
