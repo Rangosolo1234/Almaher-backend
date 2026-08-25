fce8e5fac8ecad85fe30214a5502fac28ba167e2
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2026-08-26 00:55:11 +0300

    Remove unfold conf from templates

--- core/settings.py
+++ core/settings.py
@@ -104,7 +104,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "unfold.utils.context_processors.settings",
+                # "unfold.utils.context_processors.settings",
             ],
         },
     },
